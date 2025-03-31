d4bfbcc94f7ce75422c1f4dbad690850858d3751
Toni Daada <tonidaadaadagmail.com>
2025-03-31 23:12:20 +0100

    modified:   script.js     Added classes that inherit from the parent class Workout

--- script.js
+++ script.js
@@ -11,7 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+
+  id = (new Date().getTime() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat.lng]
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
+const run = new Running([39, -12], 5.2, 24, 178);
+const cycling = new Cycling([39, -12], 25, 95, 523);
+
+console.log(run, cycling);
+//////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;

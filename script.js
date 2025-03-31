ecbc0e349e130a25ea180d343dc316a43403aa22
Toni Daada <tonidaadaadagmail.com>
2025-03-31 18:47:12 +0100

    Refactored code by Implementing classes

--- script.js
+++ script.js
@@ -11,16 +11,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
-
+let map, mapEvent;
 class App {
-  constructor() {}
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+
+    // Handles submit on map forms
+    form.addEventListener('submit', this._newWorkOut.bind(this));
+
+    // Listens  to the input change and toggles the form__row--hidden class. So one is always showing while the other is hidden
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
   _getPosition() {
     if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(this._loadMap(), function () {
-        alert(`Could not find your position`);
-      });
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`Could not find your position`);
+        }
+      );
   }
 
   _loadMap(position) {
@@ -30,64 +42,57 @@ class App {
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
-
-    map = L.map('map').setView(coords, 16);
+    this.#map = L.map('map').setView(coords, 16);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
       foo: 'bar',
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     // Adding popup to the map
     // Handles clicks on map
 
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-    });
+    this.#map.on('click', this._showForm.bind(this));
   }
 
-  _showForm() {}
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  _toggleElevationField() {}
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  _newWorkOut() {}
+  _newWorkOut(e) {
+    e.preventDefault();
+
+    // Clear inputs
+    inputType.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputDistance.value =
+        '';
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng], {
+      draggable: false,
+    })
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
 const app = new App();
-
-// Handles submit on map forms
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Clear inputs
-  inputType.value =
-    inputCadence.value =
-    inputDuration.value =
-    inputDistance.value =
-      '';
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng], {
-    draggable: false,
-  })
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-// Listens  to the input change and toggles the form__row--hidden class. So one is always showing while the other is hidden
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});

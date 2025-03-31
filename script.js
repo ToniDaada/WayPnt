d205f6fbca29b623a319ac00ba55a632f09cc1e0
Toni Daada <tonidaadaadagmail.com>
2025-03-31 17:45:00 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -11,32 +11,71 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+let mapEvent;
+
 // Using the geolocation API
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 16);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+        foo: 'bar',
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords).addTo(map).bindPopup(`Your location`).openPopup();
-
       // Adding popup to the map
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng]).addTo(map);
+      // Handles clicks on map
+
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
       alert(`Could not find your position`);
     }
   );
+// Handles submit on map forms
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear inputs
+  inputType.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputDistance.value =
+      '';
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng], {
+    draggable: false,
+  })
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+// Listens  to the input change and toggles the form__row--hidden class. So one is always showing while the other is hidden
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
